f6021f4ba095481dcb573460109edd6a11da8cf7
Magin George <magingeorge@gmail.com>
2024-02-22 14:41:14 +0530

    Adding Markers

--- src/screens/MapsScreen.jsx
+++ src/screens/MapsScreen.jsx
@@ -6,20 +6,32 @@ import "../styles/map.css";
 
 const MapsScreen = () => {
 
+    const markers = [
+        { lat: 18.5204, lng: 73.8567 },
+        { lat: 18.5314, lng: 73.8446 },
+        { lat: 18.5642, lng: 73.7769 },
+    ];
+
+    const onLoad = (map) => {
+        const bounds = new google.maps.LatLngBounds();
+        markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+        map.fitBounds(bounds);
+    };
+
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: 'AIzaSyAd235ZXhYOAPNZKrNxgGu5g3ZKef9TiRg',
     });
-    const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+    //const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
     return (
         <div className="App">
             {!isLoaded ? (
                 <h1>Loading...</h1>
             ) : (
-                <GoogleMap
-                    mapContainerClassName="map-container"
-                    center={center}
-                    zoom={10}
-                />
+                <GoogleMap mapContainerClassName="map-container" onLoad={onLoad}>
+                    {markers.map(({ lat, lng }) => (
+                        <Marker position={{ lat, lng }} />
+                    ))}
+                </GoogleMap>
             )}
         </div>
     );
