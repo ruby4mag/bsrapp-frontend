9d7d303e36eeb309e037645c1852e408b4a490eb
Magin George <magingeorge@gmail.com>
2024-02-23 00:26:59 +0530

    ewq

--- src/screens/MapsScreen.jsx
+++ src/screens/MapsScreen.jsx
@@ -5,6 +5,8 @@ import { useGetUserActivityQuery } from '../slices/activityApiSlice'
 import "../styles/map.css";
 
 const MapsScreen = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [infoWindowData, setInfoWindowData] = useState();
 
     const markers = [];
     const { data: userActivities, isLoading, error, status } = useGetUserActivityQuery({}, {
@@ -33,23 +35,39 @@ const MapsScreen = () => {
             ) : (
                 <>
                     {userActivities && (
-                        <GoogleMap mapContainerClassName="map-container" onLoad={onLoad}>
+                        <GoogleMap mapContainerClassName="map-container" onLoad={onLoad} onClick={() => setIsOpen(false)}>
 
                             <>
 
                                 {(userActivities.map((act) => {
                                     if (act.start_latlng.length != 0) {
-                                        markers.push({ lat: act.start_latlng[0], lng: act.start_latlng[1] })
+                                        markers.push({ lat: act.start_latlng[0], lng: act.start_latlng[1], title: act.name })
                                     }
                                 }))}
                                 {
-                                    markers.map(({ lat, lng }) => (
-                                        <Marker position={{ lat, lng }} />
+                                    markers.map(({ lat, lng, title }, ind) => (
+                                        <Marker
+                                            position={{ lat, lng }}
+                                            key={ind}
+                                            onClick={() => {
+                                                handleMarkerClick(ind, lat, lng, title);
+                                            }}
+
+                                        >
+                                            {isOpen && infoWindowData?.id === ind && (
+                                                <InfoWindow
+                                                    onCloseClick={() => {
+                                                        setIsOpen(false);
+                                                    }}
+                                                >
+                                                    <h3>{infoWindowData.title}</h3>
+                                                </InfoWindow>
+                                            )}
+                                        </Marker>
                                     ))
                                 }
                                 onlo
                             </>
-                            )
 
                         </GoogleMap>
                     )
