2c8ffbb139fd8ecb5d8dce81e427e0b6217d2ef0
Magin George <magingeorge@gmail.com>
2024-02-22 23:43:55 +0530

    das

--- src/screens/MapsScreen.jsx
+++ src/screens/MapsScreen.jsx
@@ -10,7 +10,7 @@ const MapsScreen = () => {
     const { data: userActivities, isLoading, error, status } = useGetUserActivityQuery({}, {
         pollingInterval: 15000,
         skip: false,
-        refetchOnMountOrArgChange: true
+        refetchOnMountOrArgChange: false
     })
 
     useEffect(
