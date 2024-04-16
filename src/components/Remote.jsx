81e5f7bb3130a3744990c025b9e28f1759412195
Magin George <magingeorge@gmail.com>
2024-04-16 19:33:31 +0530

    sad

--- src/components/Remote.jsx
+++ src/components/Remote.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Remote() {
+export default function Remote(activity) {
+
+    const [content, setContent] = useState("Initial")
+
+    useEffect(() => {
+        axios.get(`https://doapi.bsrsport.org/api/rules`, {
+            withCredentials: true
+        })
+            .then(async function (response) {
+                console.log(response.data)
+                setContent(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [])
+
+
+
+
     return (
         <>
-            MAGIN GEORGE
+            {content}
         </>
     )
 }
