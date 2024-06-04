1a428b565f3f4e110a4b7ba81fac18f83ec036f6
Magin George <magingeorge@gmail.com>
2024-06-04 22:36:22 +0530

    dsad

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -22,7 +22,7 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 const layout = [
     { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
     { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
-    { i: "kuriboh", x: 2, y: 0, w: 1, h: 1 },
+    { i: "kuriboh", x: 2, y: 0, w: 4, h: 1 },
     { i: "spell-caster", x: 3, y: 0, w: 1, h: 1 },
     { i: "summoned-skull", x: 4, y: 0, w: 1, h: 1 }
 ];
