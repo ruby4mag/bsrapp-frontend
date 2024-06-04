503a4adf36a6b6db27f300ab689bedaac8668d0d
Magin George <magingeorge@gmail.com>
2024-06-04 22:33:10 +0530

    dsa

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -18,6 +18,7 @@ import BottomRightHandle from "../components/CustomResizeHandle";
 import { Responsive, WidthProvider } from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
+
 const layout = [
     { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
     { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
@@ -467,6 +468,7 @@ export default function HomeScreen() {
                     breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                     cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
                     rowHeight={300}
+                    isResizable={true}
                     resizeHandles={["se"]}
                     resizeHandle={<BottomRightHandle />}
                     width={1000}>
