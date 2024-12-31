3b29f99bbbf6700788e09eb678e8522c8e4e0427
Magin George <magingeorge@gmail.com>
2024-12-31 10:08:22 +0530

    dsad

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -48,7 +48,7 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 var initial = true
 
 const layout = [
-    { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1, static: true },
+    { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
     { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
     { i: "kuriboh", x: 2, y: 0, w: 4, h: 1 },
     { i: "spell-caster", x: 3, y: 0, w: 1, h: 1 },
@@ -556,7 +556,7 @@ export default function HomeScreen() {
                         draggable={false}
                         resizeHandles={["se"]}
                         onLayoutChange={handleLayoutChange}
-                        draggableHandle=".drag-handle"
+                        draggableHandle='Drag'
                     //width={1000}
                     >
 
